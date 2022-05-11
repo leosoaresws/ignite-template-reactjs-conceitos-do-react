bd9465ec4d55f5e11f7132af9571ad19f64b8477
Leonardo Soares <leonardo.soares.ws@gmail.com>
2022-05-11 18:26:43 -0300

    alterando os métodos para conseguir adicionar, remover ou marcar como concluida uma Task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import '../styles/tasklist.scss'
+import "../styles/tasklist.scss";
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from "react-icons/fi";
 
 interface Task {
   id: number;
@@ -12,18 +12,52 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  // TODO: Remove quando for integrar com o banco de dados
+  // Não é a melhor estratégia para gerar um id único,
+  // mas queria resolver o desafio sem instalar nenhuma lib adicional.
+  function getUniqueRandomId(size: number): number {
+    let randomId = Math.floor(Math.random() * size);
+
+    var found = true;
+    while (found) {
+      for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id == randomId) {
+          randomId = Math.floor(Math.random() * size);
+          break;
+        }
+      }
+      found = false;
+    }
+    console.log(randomId);
+    return randomId;
+  }
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.trim().length > 0) {
+      const id = getUniqueRandomId(10000);
+      setTasks([...tasks, { id: id, title: newTaskTitle, isComplete: false }]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let tasksClone = [...tasks];
+    tasksClone.map((task: Task) => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+    });
+
+    setTasks(tasksClone);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    let tasksClone = tasks.filter((task: Task) => {
+      return task.id != id;
+    });
+
+    setTasks(tasksClone);
   }
 
   return (
@@ -32,25 +66,30 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
-          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+          <button
+            type="submit"
+            data-testid="add-task-button"
+            onClick={handleCreateNewTask}>
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
 
       <main>
         <ul>
-          {tasks.map(task => (
+          {tasks.map((task) => (
             <li key={task.id}>
-              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+              <div
+                className={task.isComplete ? "completed" : ""}
+                data-testid="task">
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -61,14 +100,16 @@ export function TaskList() {
                 <p>{task.title}</p>
               </div>
 
-              <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+              <button
+                type="button"
+                data-testid="remove-task-button"
+                onClick={() => handleRemoveTask(task.id)}>
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
-  )
+  );
 }
